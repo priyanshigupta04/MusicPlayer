6cef873068031b645e9aff4fdcc063bf0751b698
priyanshigupta04 <prigupta124@gmail.com>
2024-12-27 14:53:58 +0530

    changes for versel error

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STSTICFILES_DIRS = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles','statics')
